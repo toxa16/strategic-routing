d0207274fae03e862fcc416ffe77133d333baf34
Anton <toxa16@gmail.com>
2016-12-12 19:00:13 +0200

    Initial commit

--- index.ts
+++ index.ts
@@ -6,5 +6,6 @@ export {Post} from './lib/endpoint-decorators/post';
 export {required} from './lib/parameter-decorators/required';
 export {pattern} from './lib/parameter-decorators/pattern';
 export {minLength} from './lib/parameter-decorators/min-length';
+
 export * from './lib/errors';
 export {launch} from './lib/http-server';
